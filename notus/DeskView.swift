57e9f68caa23fef54f84dd1c5abe8db3fb3cb9a3
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2025-06-17 21:34:43 -0400

    add a NoteView for the canvas, toolpicker tbd

--- notus/DeskView.swift
+++ notus/DeskView.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import PencilKit
+
+struct NoteView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIViewType {
+        let note_canvas = PKCanvasView()
+        note_canvas.drawingPolicy = .pencilOnly
+        note_canvas.tool = PKInkingTool(.pen, color: .black, width: 10)
+        
+        let tool_picker = PKToolPicker()
+        tool_picker.addObserver(note_canvas)
+        tool_picker.setVisible(true, forFirstResponder: note_canvas)
+        
+        return note_canvas
+    }
+    
+    func updateUIView(_ type: UIViewType, context: Context) {
+        
+    }
+    
+    static func dismantleUIView(_ uiView: PKCanvasView, coordinator: ()) {
+        
+    }
+    
+    typealias UIViewType = PKCanvasView
+}
 
 struct DeskView: View {
     var on_file_away: () -> Void
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NoteView()
     }
 }
 
