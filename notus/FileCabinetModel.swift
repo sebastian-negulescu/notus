d37dd817857e3b753866962ead0abf533d52fa6a
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2025-07-05 21:52:42 -0400

    simplify the FileCabinet model

--- notus/FileCabinetModel.swift
+++ notus/FileCabinetModel.swift
@@ -7,73 +7,131 @@
 
 import Foundation
 import PencilKit
+import os
+
+struct Note {
+    var name: String
+    var contents: PKDrawing
+}
+
+struct Folder {
+    var name: String
+    var contents: [String]
+}
+
 
 class FileCabinet {
-    var root: Folder
-    var current_directory: Folder
-    var path: [Int]
-    var current_note: CabinetItem?
+    var desk: Note? = nil
+    
+    private let file_manager: FileManager = FileManager.default
+    private var open_folder: URL
     
     init() {
-        root = Folder(name: "")
-        current_directory = root
-        path = []
-        current_note = nil
+        open_folder = file_manager.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "notus.data", directoryHint: .isDirectory)
     }
     
-    func select_item(name: String) -> CabinetItem {
-        assert(Folder.name_exists(folder: current_directory, name: name))
-        
-        for (ind, cabinet_item) in current_directory.notes.enumerated() {
-            if cabinet_item.name() == name {
-                switch cabinet_item.item_type {
-                case .Folder:
-                    current_directory = cabinet_item.folder!
-                    path.append(ind)
-                case .NoteBook, .NotePad:
-                    current_note = cabinet_item
-                }
-                return cabinet_item
-            }
-        }
-        
-        assert(false)
+    private func get_note_path(_ name: String) -> URL {
+        return open_folder.appending(path: name, directoryHint: .notDirectory)
     }
     
-    @discardableResult
-    static func new_item(folder: Folder, name: String, item_type: CabinetItemType) -> Bool {
-        let item = CabinetItem(item_type: item_type)
-        switch item_type {
-        case .Folder:
-            item.folder = Folder(name: name, notes: [])
-        case .NotePad:
-            item.note_pad = NotePad(name: name, page: PKDrawing())
-        case .NoteBook:
-            item.note_book = NoteBook(name: name, pages: [PKDrawing()])
+    // TODO: run CRUD operations in a DispatchQueue
+    // TOOD: make CRUD functions generic
+    
+    func create_folder(name: String) -> Bool {
+        let path = open_folder.appending(path: name, directoryHint: .isDirectory)
+        do {
+            try file_manager.createDirectory(at: path, withIntermediateDirectories: false)
+            self.open_folder = path
+            return true
+        } catch {
+            os_log("Could not create folder: %s", type: .error, error.localizedDescription)
         }
-        
-        if Folder.name_exists(folder: folder, name: name) {
-            return false
-        }
-        folder.notes.append(item)
-        return true
+        return false
     }
     
-    func generate_path() -> String {
-        var path_string: String = "/"
-        var path_folder = root
-        
-        for ind in path {
-            path_folder = path_folder.notes[ind].folder!
-            path_string += path_folder.name + "/"
+    func read_folder() -> [String]? {
+        do {
+            let contents = try file_manager.contentsOfDirectory(atPath: open_folder.absoluteString)
+            return contents
+        } catch {
+            os_log("Could not read folder: %s", type: .error, error.localizedDescription)
         }
         
-        return path_string
+        return nil
     }
     
-    func parent() -> Void {
-        if !path.isEmpty {
-            path.removeLast()
+    func delete_folder(name: String) -> Bool {
+        do {
+            try file_manager.removeItem(at: open_folder.appending(path: name, directoryHint: .isDirectory))
+        } catch {
+            os_log("Could not delete folder: %s", type: .error, error.localizedDescription)
         }
+        return false
+    }
+    
+    func ascend_folder() -> [String]? {
+        let open_folder_backup = self.open_folder
+        self.open_folder = open_folder.deletingLastPathComponent()
+        if let contents: [String] = read_folder() {
+            return contents
+        }
+        self.open_folder = open_folder_backup
+        return read_folder()
+    }
+    
+    func descend_folder(name: String) -> [String]? {
+        let open_folder_backup = self.open_folder
+        self.open_folder = open_folder.appending(path: name, directoryHint: .isDirectory)
+        if let contents: [String] = read_folder() {
+            return contents
+        }
+        self.open_folder = open_folder_backup
+        return read_folder()
+    }
+    
+    func create_note(name: String) -> Bool {
+        let new_note = PKDrawing()
+        
+        do {
+            try new_note.dataRepresentation().write(to: get_note_path(name))
+            return true
+        } catch {
+            os_log("Could not create note: %s", type: .error, error.localizedDescription)
+        }
+        
+        return false
+    }
+    
+    func read_note(name: String) -> Note? {
+        do {
+            let note = Note(name: name, contents: try PKDrawing(data: Data(contentsOf: get_note_path(name))))
+            return note
+        } catch {
+            os_log("Could not load note: %s", type: .error, error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+    func update_note(note: Note) -> Bool {
+        do {
+            try note.contents.dataRepresentation().write(to: get_note_path(note.name))
+            return true
+        } catch {
+            os_log("Could not update note: %s", type: .error, error.localizedDescription)
+        }
+        
+        return false
+    }
+    
+    func delete_note(name: String) -> Bool {
+        do {
+            try file_manager.removeItem(at: get_note_path(name))
+            return true
+        } catch {
+            os_log("Could not delete note: %s", type: .error, error.localizedDescription)
+        }
+        
+        return false
     }
 }
