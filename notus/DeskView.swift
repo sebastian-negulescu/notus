f41dbc735082390c1330558db7e5d037397f4e2d
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2026-03-14 15:39:45 -0400

    move tab gestures to TabView

--- notus/DeskView.swift
+++ notus/DeskView.swift
@@ -8,11 +8,7 @@
 import SwiftUI
 import PencilKit
 
-extension Comparable {
-    func clamped(to limits: ClosedRange<Self>) -> Self {
-        return min(max(self, limits.lowerBound), limits.upperBound)
-    }
-}
+
 
 struct DeskView: View {
     var page: PageView
@@ -21,104 +17,12 @@ struct DeskView: View {
         self.page = PageView(drawing: PKDrawing())
     }
     
-    @State var red_tab_x: CGFloat = 0
-    @State var red_tab_offset: CGSize = .zero
-    @State var green_tab_x: CGFloat = 0
-    @State var green_tab_offset: CGSize = .zero
-    @State var blue_tab_x: CGFloat = 0
-    @State var blue_tab_offset: CGSize = .zero
-    
-    
     var body: some View {
         ZStack(alignment: .topTrailing) {
             page
-            TabView(colour: .red, content: Text("Hello, world!"))
-                .offset(x: (500 + (red_tab_offset.width + red_tab_x)).clamped(to: CGFloat(0)...CGFloat(500)))
-                .gesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            red_tab_offset = gesture.translation
-                        }
-                        .onEnded { gesture in
-                            withAnimation(.spring(duration: 0.2, bounce: 0.1)) {
-                                let displacement = abs(gesture.predictedEndLocation.x - gesture.startLocation.x)
-                                // true is right, false is left
-                                let direction = gesture.location.x > gesture.startLocation.x
-                                if (displacement > 150) {
-                                    if (red_tab_x == -500 && direction) {
-                                        red_tab_x = 0
-                                    } else if (!direction) {
-                                        red_tab_x = -500
-                                    }
-                                } else {
-                                    if (red_tab_x == 0) {
-                                        red_tab_x = 0
-                                    } else {
-                                        red_tab_x = -500
-                                    }
-                                }
-                                red_tab_offset = .zero
-                            }
-                        }
-                )
-            TabView(colour: .green, content: Text("Goodbye, world!"))
-                .offset(x: (500 + (green_tab_offset.width + green_tab_x)).clamped(to: CGFloat(0)...CGFloat(500)), y: 50)
-                .gesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            green_tab_offset = gesture.translation
-                        }
-                        .onEnded { gesture in
-                            withAnimation(.spring(duration: 0.2, bounce: 0.1)) {
-                                let displacement = abs(gesture.predictedEndLocation.x - gesture.startLocation.x)
-                                // true is right, false is left
-                                let direction = gesture.location.x > gesture.startLocation.x
-                                if (displacement > 150) {
-                                    if (green_tab_x == -500 && direction) {
-                                        green_tab_x = 0
-                                    } else if (!direction) {
-                                        green_tab_x = -500
-                                    }
-                                } else {
-                                    if (green_tab_x == 0) {
-                                        green_tab_x = 0
-                                    } else {
-                                        green_tab_x = -500
-                                    }
-                                }
-                                green_tab_offset = .zero
-                            }
-                        }
-                )
-            TabView(colour: .blue, content: Text("Goodbye, world!"))
-                .offset(x: (500 + (blue_tab_offset.width + blue_tab_x)).clamped(to: CGFloat(0)...CGFloat(500)), y: 100)
-                .gesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            blue_tab_offset = gesture.translation
-                        }
-                        .onEnded { gesture in
-                            withAnimation(.spring(duration: 0.2, bounce: 0.1)) {
-                                let displacement = abs(gesture.predictedEndLocation.x - gesture.startLocation.x)
-                                // true is right, false is left
-                                let direction = gesture.location.x > gesture.startLocation.x
-                                if (displacement > 150) {
-                                    if (blue_tab_x == -500 && direction) {
-                                        blue_tab_x = 0
-                                    } else if (!direction) {
-                                        blue_tab_x = -500
-                                    }
-                                } else {
-                                    if (blue_tab_x == 0) {
-                                        blue_tab_x = 0
-                                    } else {
-                                        blue_tab_x = -500
-                                    }
-                                }
-                                blue_tab_offset = .zero
-                            }
-                        }
-                )
+            TabView(tab_offset: 0, colour: .red, content: Text("Hello, world!"))
+            TabView(tab_offset: 1, colour: .green, content: Text("Hello, world!"))
+            TabView(tab_offset: 2, colour: .blue, content: Text("Hello, world!"))
         }
     }
 }
---
+++ notus/Utils.swift
@@ -0,0 +1,16 @@
+//
+//  Utils.swift
+//  notus
+//
+//  Created by Sebastian Negulescu on 2026-03-14.
+//
+
+extension Comparable {
+    func clamped(to limits: ClosedRange<Self>) -> Self {
+        return min(max(self, limits.lowerBound), limits.upperBound)
+    }
+}
+
+func almost_equal<T:FloatingPoint>(_ a:T, _ b:T) -> Bool {
+    return b.nextDown ... b.nextUp ~= a
+}
